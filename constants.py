e8134dfb0e80acc0e807927c8fed7768f1b9821c
Tanveer Najib <tanveernajib@gmail.com>
2025-09-07 18:47:49 +0100

    Bump version

--- constants.py
+++ constants.py
@@ -3,7 +3,7 @@ import os
 
 from internal.session import Session
 
-APP_VERSION = "0.0.7"
+APP_VERSION = "0.0.8"
 DATA_DIR = platformdirs.user_config_dir("gpb-frankenstein")
 MATCHES_FILE = os.path.join(DATA_DIR, "matches.json")
 LOG_FILE = os.path.join(DATA_DIR, f"frankenstein_{APP_VERSION}.log")
